d6c4abb2decdcecae75a347608c2ea479b8795e0
Yury Karytsin <karytsin.yury@gmail.com>
2015-01-18 01:57:47 +0300

    Sort selection alghorithm is added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,55 @@
-#include <QCoreApplication>
+#include <algorithm>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+template <class Type>
+void initArray(Type *array, int length);
+
+template <class Type>
+void showArray(Type *array, int length);
+
+template <class Type>
+void sortSelection(Type *array, int begin, int end);
+
+int main()
 {
-    QCoreApplication a(argc, argv);
+    int array[20];
 
-    return a.exec();
+    initArray<int>(array, 20);
+    showArray<int>(array, 20);
+
+    cout << endl;
+
+    sortSelection<int>(array, 0, 20);
+    showArray<int>(array, 20);
+    return 0;
+}
+
+template <class Type>
+void initArray(Type *array, int length) {
+    for(int index = 0; index < length; index++)
+        array[index] = rand() % length;
+}
+
+template <class Type>
+void showArray(Type *array, int length) {
+    for(int index = 0; index < length; index++) {
+        if(index % 10 == 0)
+            cout << "\n";
+        cout << setw(6) << array[index];
+    }
+}
+
+template <class Type>
+void sortSelection(Type *array, int begin, int end) {
+    for(int i = begin; i < end - 1; i++) {
+        int min = i;
+        for(int j = i + 1; j < end; j++)
+            if(array[j] < array[min])
+                min = j;
+        swap(array[i], array[min]);
+    }
 }
